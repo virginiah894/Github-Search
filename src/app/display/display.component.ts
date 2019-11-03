a30e0a34e465a107a8a848772ca47fdf20cb2f58
virginiah894 <virgyperry@gmail.com>
2019-11-03 11:01:32 +0300

    add logic to the display component using the user class

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import{ HttpClient} from "@angular/common/http"
-import{ UserService} from "../user-service/user.service";
+// import{ HttpClient} from "@angular/common/http"
+// import{ UserService} from "../user-service/user.service";
 import{ User} from "../user";
 @Component({
   selector: 'app-display',
@@ -8,22 +8,35 @@ import{ User} from "../user";
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
-users:User[];
-  constructor(public userService:UserService, private Http:HttpClient) { }
+users:User=[
+ { name:"Virginiah"},
+
+ { name:"Virginiah"},
+
+ { name:"Virginiah"},
+
+ { name:"Virginiah"}
+
+
+];
+
+
+
+  // constructor(public userService:UserService, private Http:HttpClient) { }
 
   ngOnInit() {
-    this.searchUser("virginiah894");
+    // this.searchUser("virginiah894");
   }
 
-  searchUser(searchTerm){
-    this.userService.searchUser(searchTerm).then(
-      ()=>{
-        this.users=this.userService.users;
-      },
-      (error)=>{
-        console.log(error)
-      }
-    )
-    }
+  // searchUser(searchTerm){
+  //   this.userService.searchUser(searchTerm).then(
+  //     ()=>{
+  //       this.users=this.userService.users;
+  //     },
+  //     (error)=>{
+  //       console.log(error)
+  //     }
+  //   )
+  //   }
   
-  }
+  // }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,6 +1,6 @@
 export class User {
   // constructor(public url:string)
-  constructor(public name:string,public any:string,public date:Date){
+  constructor(public name:string){
     
   }
 }
